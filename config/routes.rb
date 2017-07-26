7ca1e92e21f14d290a7778a331fb61f3b3b1243a
Tony Wang <sspy45@gmail.com>
2017-07-25 23:37:56 -0700

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome_page/welcome'
+  root 'welcome_page/welcome'
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
